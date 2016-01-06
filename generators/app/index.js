2047b4abc046a8caf8caae14aebf48a75e8d181f
Jacob Greenleaf <greenleaf.jacob@gmail.com>
2016-01-05 19:21:33 -0500

    Style; Add defaults for some prompts

--- generators/app/index.js
+++ generators/app/index.js
@@ -2,95 +2,101 @@
 var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+var process = require('process');
+var path = require('path');
 
 module.exports = yeoman.generators.Base.extend({
-    prompting: function () {
-        var done = this.async();
+  prompting: function () {
+      var done = this.async();
 
         // Have Yeoman greet the user.
-        this.log(yosay(
+      this.log(yosay(
             'Welcome to the ' + chalk.red('generator-vagrantchef') + ' generator!'
         ));
 
-        var prompts = [{
-            type: 'input',
-            name: 'name',
-            message: 'What is this project\'s name?'
-        },{
-            type: 'input',
-            name: 'syncPath',
-            message: 'Synced folder path? Where, in the virtual machine, should the root folder of this project be mounted?'
-        },{
-            type: 'input',
-            name: 'language',
-            message: 'Travis-CI language?'
+      var prompts = [{
+          type: 'input',
+          name: 'name',
+          message: 'What is this project\'s name?',
+          default: path.basename(process.cwd())
         }, {
-            type: 'input',
-            name: 'maintainer',
-            message: 'Who is the maintainer of this project?'
+          type: 'input',
+          name: 'syncPath',
+          message: 'Synced folder path? Where, in the virtual machine, should the root folder of this project be mounted?',
+          default: '/' + path.basename(process.cwd())
+        }, {
+          type: 'input',
+          name: 'language',
+          message: 'Travis-CI language?',
+          default: 'ruby'
+        }, {
+          type: 'input',
+          name: 'maintainer',
+          message: 'Who is the maintainer of this project?',
+          default: 'Joe Smith <jsmith@gmail.com>'
         }];
 
-        this.prompt(prompts, function (props) {
-            this.props = props;
+      this.prompt(prompts, function (props) {
+          this.props = props;
             // To access props later use this.props.someOption;
 
-            done();
+          done();
         }.bind(this));
     },
 
-    writing: function () {
-        this.fs.copyTpl(
+  writing: function () {
+      this.fs.copyTpl(
             this.templatePath('Vagrantfile'),
             this.destinationPath('Vagrantfile'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('Berksfile'),
             this.destinationPath('cookbooks/Berksfile'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('.gitignore'),
             this.destinationPath('.gitignore'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('travis.yml'),
             this.destinationPath('.travis.yml'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('metadata.rb'),
             this.destinationPath('cookbooks/' + this.props.name + '/metadata.rb'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('recipe-default.rb'),
             this.destinationPath('cookbooks/' + this.props.name + '/recipes/default.rb'),
             this.props
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('template-demo.erb'),
             this.destinationPath('cookbooks/' + this.props.name + '/templates/demo.erb'),
             this.props,
-            {
-                delimiter: '?'
-            }
+          {
+            delimiter: '?'
+          }
         );
 
-        this.fs.copyTpl(
+      this.fs.copyTpl(
             this.templatePath('attributes.rb'),
             this.destinationPath('cookbooks/' + this.props.name + '/attributes/' + this.props.name + '.rb'),
             this.props
         );
     },
 
-    install: function () {
+  install: function () {
     }
 });
--- test/app.js
+++ test/app.js
@@ -4,27 +4,27 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
 describe('generator-vagrantchef:app', function () {
-    before(function (done) {
-        helpers.run(path.join(__dirname, '../generators/app'))
+  before(function (done) {
+      helpers.run(path.join(__dirname, '../generators/app'))
             // .withOptions({someOption: true})
             .withPrompts({
-                name: 'foobar',
-                syncPath: '/path',
-                language: 'go',
-                maintainer: 'Jacob Greenleaf'
+              name: 'foobar',
+              syncPath: '/path',
+              language: 'go',
+              maintainer: 'Jacob Greenleaf'
             })
         .on('end', done);
     });
 
-    it('creates files', function () {
-        assert.file([
-                'Vagrantfile',
-                '.travis.yml',
-                'cookbooks/Berksfile',
-                'cookbooks/foobar/metadata.rb',
-                'cookbooks/foobar/attributes/foobar.rb',
-                'cookbooks/foobar/recipes/default.rb',
-                'cookbooks/foobar/templates/demo.erb'
+  it('creates files', function () {
+      assert.file([
+          'Vagrantfile',
+          '.travis.yml',
+          'cookbooks/Berksfile',
+          'cookbooks/foobar/metadata.rb',
+          'cookbooks/foobar/attributes/foobar.rb',
+          'cookbooks/foobar/recipes/default.rb',
+          'cookbooks/foobar/templates/demo.erb'
         ]);
     });
 });
